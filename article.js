70b2fa2f22ff6ac706f85300e9a3c66d908187e8
Arthur Jamain <a.jamain.pro@gmail.com>
2025-06-02 13:29:32 +0200

    Fix date selector + Map link

--- article.js
+++ article.js
@@ -1,53 +1,76 @@
-async function applyOldPrice4Article(article, postId) {
+async function applyOldPrice4Article(article) {
+    const postId = getPostId();
+    if (!!currentPostId && currentPostId != postId) {
+        location.reload();
+        return;
+    }
+    currentPostId = postId;
     const datas = await getApiData(postId);
     const oldDate = datas?.first_publication_date
     
-    const currentDate = datas?.index_date;
-    if (oldDate) {    
-        displayOldDateInElement(article, postId, oldDate, currentDate);
-    } else {
-        displayOldDateInElement(article, postId, currentDate, currentDate)
-    }
+    try {
+        const currentDate = datas?.index_date;
+        if (oldDate) {    
+            displayOldDateInElement(article, postId, oldDate, currentDate);
+        } else {
+            displayOldDateInElement(article, postId, currentDate, currentDate)
+        }
+    } catch (e) {err(e)}
     
-    const currentPrice = datas?.price[0];
-    const oldPrice = datas?.attributes?.filter(o => o.key === 'old_price')[0]?.value
+    try {
+        const currentPrice = datas?.price[0];
+        const oldPrice = datas?.attributes?.filter(o => o.key === 'old_price')[0]?.value
 
-    if (oldPrice) {
-        displayOldPriceInElement(article, postId, oldPrice, currentPrice);
-    } else {
-        displayCurrentPriceInElement(article, postId, currentPrice);
-    }
+        if (oldPrice) {
+            displayOldPriceInElement(article, postId, oldPrice, currentPrice);
+        } else {
+            displayCurrentPriceInElement(article, postId, currentPrice);
+        }
+    } catch (e) {err(e)}
     
     try {
         enhanceArticleDescriptionDisplay(article);
-    } catch (e) {console.error(e)}
+    } catch (e) {err(e)}
     try {
         enhanceArticleCritereDisplay(article, datas);
-    } catch (e) {console.error(e)}
+    } catch (e) {err(e)}
     try {
         enhanceAdviewSticky();
-    } catch (e) {console.error(e)}
+    } catch (e) {err(e)}
+
     try {
         movePackSerenite(article);
-    } catch (e) {console.error(e)}
+    } catch (e) {err(e)}
     try {
         moveAutoviza(article);
-    } catch (e) {console.error(e)}
+    } catch (e) {err(e)}
+    try {
+        moveProtection(article);
+    } catch (e) {err(e)}
 }
 
 function displayOldDateInElement(element, id, oldDate, currentDate) {
     const exist = element.querySelector('[id^="old_date_to_display_"]');
     if (exist) {
         exist.parentElement.removeChild(exist);
+        element.querySelector('[old_date_to_redisplay="true"]')?.removeAttribute("style");
     }
 
-    const dateContainer = element.querySelector('[data-qa-id="adview_spotlight_description_container"] p.text-caption.opacity-dim-1').parentElement;
-    const currentDateClass = dateContainer.firstChild.classList;
+    const dateContainer = Array.from(document.querySelectorAll('[data-qa-id="adview_spotlight_description_container"] p.text-caption'))?.find(el => / à \d{2}:\d{2}/.test(el.textContent))?.parentElement;
+
+    if (!dateContainer) {
+        err('Cannot find date Container');
+        return;
+    }
+
+    const oldDateDisplay = dateContainer.firstChild;
+    const currentDateClass = oldDateDisplay.classList;
 
     const divOldDate = createDivOldDate(id, currentDateClass, oldDate, currentDate);
 
-    dateContainer.firstChild.setAttribute("style", "display:none");
-    dateContainer.insertBefore(divOldDate, dateContainer.firstChild);
+    oldDateDisplay.setAttribute("old_date_to_redisplay","true")
+    oldDateDisplay.setAttribute("style", "display:none");
+    dateContainer.insertBefore(divOldDate, oldDateDisplay);
 }
 
 function enhanceArticleDescriptionDisplay(article) {
@@ -57,16 +80,7 @@ function enhanceArticleDescriptionDisplay(article) {
         const splitChar = " • ";
         const oldDesc = description.innerHTML.split(splitChar);
 
-        const pin = article.querySelector("[data-title='PinOutline']");
-        var place;
-
-        if (pin) {
-            place = `<a id="goToMap" class="underline inline-flex" title="Aller à la carte">`
-            + pin.outerHTML.replace("fill-current text-current w-sz-16 h-sz-16 mr-sm mt-sm", "w-sz-16 h-sz-16 mr-sm inline-block")
-            + oldDesc[0] + '</a>';
-        } else {
-            place = oldDesc[0];
-        }
+        var place = `<a id="goToMap" class="underline inline-flex" title="Aller à la carte">${getPin() + oldDesc[0]}</a>`;
 
         description.innerHTML = 
         place
@@ -76,9 +90,12 @@ function enhanceArticleDescriptionDisplay(article) {
         + (oldDesc[4] ? splitChar + oldDesc[4] : "")
         + (oldDesc[5] ? splitChar + oldDesc[5] : "");
 
-        if (pin) {
-            document.getElementById("goToMap").onclick = () => {document.getElementById("map").scrollIntoView({ behavior: "smooth"});};
-        }
+        document.getElementById("goToMap").onclick = () => {
+            window.scrollTo({
+                behavior: 'smooth',
+                top: document.getElementsByClassName("LazyLoad")[0].getBoundingClientRect().top - document.body.getBoundingClientRect().top - 56,
+            });
+        };
     }
 }
 
@@ -92,6 +109,8 @@ function enhanceArticleCritereDisplay(article, datas) {
     }
 
     if (critereKm) {
+        critereKm.querySelector("div").innerHTML = spaceDigits(critereKm.querySelector("div").innerHTML);
+
         const kmPan = critereKm.cloneNode(true);
         
         const exist = article.querySelector("[data-qa-id='criteria_monthly_mileage']")
@@ -101,10 +120,9 @@ function enhanceArticleCritereDisplay(article, datas) {
 
         if (kmAge && dateMes && mDiff) {
             kmPan.setAttribute("data-qa-id", "criteria_monthly_mileage")
-            kmPan.innerHTML = spaceDigits(kmPan.innerHTML.replaceAll(`${kmAge} km`, `${Math.round(kmAge/mDiff)} km/mois`).replaceAll("Kilométrage", "Kilomètres/mois estimés"));
+            kmPan.querySelector("div").innerHTML = spaceDigits(kmPan.querySelector("div").innerHTML.replaceAll(`${spaceDigits(kmAge)} km`, `${Math.round(kmAge/mDiff)} km/mois`).replaceAll("Kilométrage", "Kilomètres/mois estimés"));
             critereKm.parentElement.insertBefore(kmPan, critereKm.nextSibling);
         }
-        critereKm.innerHTML = spaceDigits(critereKm.innerHTML);
     }
 
     const critereDatePmes = article.querySelector("[data-qa-id='criteria_item_issuance_date']");
@@ -118,23 +136,32 @@ function enhanceArticleCritereDisplay(article, datas) {
 function moveAutoviza(article) {
     const divAutoviza = document.evaluate("//h2[contains(., 'Autoviza')]", article, null, XPathResult.ANY_TYPE, null).iterateNext()?.parentElement;
 
-    moveDivAside(article, divAutoviza);
+    moveDivAside(article, divAutoviza, "autoviza");
+}
+
+function moveProtection(article) {
+    const divProtection = document.evaluate("//section[contains(., 'Protection leboncoin')]", article, null, XPathResult.ANY_TYPE, null).iterateNext();
+
+    moveDivAside(article, divProtection, "protection");
 }
 
 function movePackSerenite(article) {
     const divPackSerenite = document.evaluate("//p[contains(., 'Pack Sérénité*')]", article, null, XPathResult.ANY_TYPE, null).iterateNext()?.parentElement?.parentElement;
 
-    moveDivAside(article, divPackSerenite);
+    moveDivAside(article, divPackSerenite, "packseren");
 }
 
-function moveDivAside(container, div) {
+function moveDivAside(container, div, type) {
     if (div) {
-        const asideFirstChild = container.querySelector("aside>div")
+        document.querySelector(`[lbc_old_price_move='${type}']`)?.remove();
+
+        div.classList.remove("py-xl","border-b-sm","border-outline")
+        const asideRefSection = container.querySelector("aside section");
         const newDiv = document.createElement("div");
-        newDiv.innerHTML = `<${asideFirstChild.firstChild.nodeName} class='${asideFirstChild.firstChild.classList}'></${asideFirstChild.firstChild.nodeName}>`;
+        newDiv.setAttribute(`lbc_old_price_move`,type)
+        newDiv.innerHTML = `<${asideRefSection.nodeName} class='${asideRefSection.classList}'></${asideRefSection.nodeName}>`;
         newDiv.firstChild.appendChild(div);
-        newDiv.firstChild.firstChild.classList.remove("py-xl")
-        asideFirstChild.after(newDiv);
+        asideRefSection.after(newDiv);
     }
 }
 
@@ -145,4 +172,8 @@ function enhanceAdviewSticky() {
         adviewSticky.classList.add("cursor-pointer")
         adviewSticky.setAttribute("style", "width: -webkit-fill-available")
     }
+}
+
+function getPin() {
+    return `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-sz-16 h-sz-16 mr-sm inline-block" data-spark-component="icon" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.3754 8.89783C15.3754 10.7038 13.8643 12.1678 12.0003 12.1678C10.1363 12.1678 8.6252 10.7038 8.6252 8.89783C8.6252 7.09187 10.1363 5.62785 12.0003 5.62785C13.8643 5.62785 15.3754 7.09187 15.3754 8.89783ZM13.3044 8.89783C13.3044 9.59562 12.7205 10.1613 12.0003 10.1613C11.2801 10.1613 10.6962 9.59562 10.6962 8.89783C10.6962 8.20004 11.2801 7.63437 12.0003 7.63437C12.7205 7.63437 13.3044 8.20004 13.3044 8.89783Z"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M12 2.00024C7.58172 2.00024 4 5.47039 4 9.75102C4 10.8868 4.41304 12.2052 4.97459 13.4754C5.5461 14.7681 6.31556 16.1078 7.12222 17.3163C7.92805 18.5235 8.78895 19.6265 9.55506 20.44C9.9359 20.8445 10.3142 21.1998 10.6676 21.4625C10.8442 21.5938 11.0353 21.7176 11.2346 21.8123C11.4223 21.9016 11.6899 22.0002 12 22.0002C12.3101 22.0002 12.5777 21.9016 12.7654 21.8123C12.9647 21.7176 13.1558 21.5938 13.3324 21.4625C13.6858 21.1998 14.0641 20.8445 14.4449 20.44C15.211 19.6265 16.0719 18.5235 16.8778 17.3163C17.6844 16.1078 18.4539 14.7681 19.0254 13.4754C19.587 12.2052 20 10.8868 20 9.75102C20 5.47039 16.4183 2.00024 12 2.00024ZM6.07104 9.75102C6.07104 6.57856 8.72552 4.00676 12 4.00676C15.2745 4.00676 17.929 6.57856 17.929 9.75102C17.929 10.4785 17.6468 11.4975 17.1217 12.6853C16.6065 13.8506 15.8978 15.0894 15.1387 16.2266C14.3788 17.3651 13.5862 18.3749 12.915 19.0877C12.5772 19.4464 12.2909 19.7076 12.0715 19.8707C12.0456 19.89 12.0218 19.907 12 19.922C11.9782 19.907 11.9544 19.89 11.9285 19.8707C11.7091 19.7076 11.4228 19.4464 11.085 19.0877C10.4138 18.3749 9.62122 17.3651 8.86127 16.2266C8.10215 15.0894 7.39349 13.8506 6.87834 12.6853C6.35322 11.4975 6.07104 10.4785 6.07104 9.75102Z"></path></svg>`;
 }

0ff4aeb36cb330ba7df9a83fb4813562ed97b5eb
Arthur Jamain <a.jamain.pro@gmail.com>
2025-09-18 19:48:07 +0200

    CI du Q mais mieux

--- release.py
+++ release.py
@@ -4,20 +4,23 @@ import os
 import json
 import zipfile
 import fnmatch
+import shutil
 
 # exclusions par nom ou pattern
 EXCLUDE = {".git", ".idea", ".vscode", "img", "releases", ".gitignore", "build"}
-PATTERN_EXCLUDE = ["*.zip", "*.bat", "*.py", "*.md", "*.exe"]
+PATTERN_EXCLUDE = ["*.zip", "*.bat", "*.py", "*.md", "*.exe", "manifest.firefox.json", "manifest.chrome.json"]
 
-def get_version():
-    with open("manifest.json", "r", encoding="utf-8") as f:
+def get_version(manifest):
+    with open(manifest, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data.get("version", "0.0.0")
 
-def zip_project(version):
+def zip_project(version, fichierManifest):
     release_dir = "."
     os.makedirs(release_dir, exist_ok=True)
     zip_name = os.path.join(release_dir, f"lbc_old_price_v{version}.zip")
+
+    shutil.copy2(fichierManifest, "manifest.json")
 
     with zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk("."):
@@ -35,8 +38,14 @@ def zip_project(version):
                 arcname = os.path.relpath(filepath, ".")
                 zipf.write(filepath, arcname)
 
+    os.remove("manifest.json")
     print(f"✅ Archive créée : {zip_name}")
 
 if __name__ == "__main__":
-    version = get_version()
-    zip_project(version)
+    ff = "manifest.firefox.json"
+    versionFF = get_version(ff)
+    zip_project(versionFF, ff)
+    
+    ch = "manifest.chrome.json"
+    versionCH = get_version(ch)
+    zip_project(versionCH, ch)
